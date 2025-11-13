0ebac127945daca1fe02a0f4fa195279d0f5fc6f
Solomon Eseme <kaperskyguru@gmail.com>
2025-11-13 16:24:08 +0100

    fix

--- components/pages/roadmap-watch.tsx
+++ components/pages/roadmap-watch.tsx
@@ -345,9 +345,9 @@ export function RoadmapWatchPage({
                 const found = course?.userCourses?.find(
                   (uc: any) => uc.courseId === course.id && uc.isRoadmap
                 );
-                console.log(found);
+
                 const completedTask = getCompletedTasks(
-                  found.id,
+                  found?.currentVideoId,
                   milestone?.userTopic?.id
                 );
 
